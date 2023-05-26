47358cc3b7a6d1c0618a79265d801e87bcd63b43
jsk28 <jsk28@sussex.ac.uk>
2023-05-27 00:20:10 +0200

    debug logs added

--- packages/shielder.js-core/src/lib.rs
+++ packages/shielder.js-core/src/lib.rs
@@ -11,6 +11,8 @@ use rand::Rng;
 use serde_json;
 use utils::*;
 use wasm_bindgen::prelude::*;
+use web_sys::console;
+
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
 #[cfg(feature = "wee_alloc")]
@@ -19,16 +21,21 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
 pub async fn deposit(deposit_data_string: String) -> String {
+    console::log_1(&"START".into());
     let pk_bytes: Vec<u8> = fetch_pk_bytes(DEPOSIT_PK_URL.to_string()).await;
-    let mut prepare_deposit_data: PrepareDeposit =
-        serde_json::from_str(&deposit_data_string).unwrap();
+    console::log_1(&format!("PK_bytes: {:?}", pk_bytes).into());
+    let prepare_deposit_data: PrepareDeposit = serde_json::from_str(&deposit_data_string).unwrap();
     let (trapdoor, nullifier) = rand::thread_rng().gen::<(FrontendTrapdoor, FrontendNullifier)>();
+    console::log_1(&format!("Trapdoor: {:?}", trapdoor).into());
+    console::log_1(&format!("Nullifier: {:?}", nullifier).into());
     let note = compute_note(
         prepare_deposit_data.token_id,
         prepare_deposit_data.token_amount,
         trapdoor,
         nullifier,
     );
+
+    console::log_1(&format!("Note: {:?}", note).into());
     let circuit = DepositRelationWithFullInput::new(
         note,
         prepare_deposit_data.token_id,
@@ -38,6 +45,7 @@ pub async fn deposit(deposit_data_string: String) -> String {
     );
     let pk = <<Groth16 as ProvingSystem>::ProvingKey>::deserialize(&*pk_bytes).unwrap();
     let proof = "0x".to_string() + hex::encode(serialize(&Groth16::prove(&pk, circuit))).as_str();
+    console::log_1(&format!("Proof: {:?}", proof).into());
     let deposit_data = Deposit {
         deposit_id: prepare_deposit_data.deposit_id,
         token_id: prepare_deposit_data.token_id,
