d36dc72270ef5eda78b1bd4cca173cc583b030b7
Kodziak <kodziak1416@gmail.com>
2023-05-27 15:01:22 +0200

    lfg

--- packages/shielder.js-sdk/src/browser.ts
+++ packages/shielder.js-sdk/src/browser.ts
@@ -14,7 +14,6 @@ async function foo() {
 
 
 interface Deposit {
-  deposit_id: number;
   token_id: number;
   token_amount: number;
   lead_idx?: number;
@@ -46,41 +45,27 @@ async function readAllChunks(readableStream: any) {
 //   }, []);
 // }
 
-async function deposit() {
+async function deposit(args: Deposit) {
   console.log('deposit');
-
-
-  console.log(JSON.stringify([[0,0], [0,1], [0,3]]));
 
   const res = await fetch('https://bafybeifjsmmivrq2hxmujww7t2t3prbu3r2vffh7bq7pf6su2v5qgmzd4u.ipfs.w3s.link/ipfs/bafybeifjsmmivrq2hxmujww7t2t3prbu3r2vffh7bq7pf6su2v5qgmzd4u/deposit.pk.bytes')
   const chunks = (await readAllChunks(res.body));
   
-  let sum = 0;
   const flatten: number[] = [];
 
   chunks.forEach(chunk => {
-    sum += chunk.length;
     flatten.push(...chunk);
   });
 
-  console.log(sum, flatten);
-
-  const dep: Deposit = {
-    deposit_id: 0,
-    token_id: 0,
-    token_amount: 100
-  }
-
-  console.log('parsed deposit', JSON.stringify(dep))
-  console.log('chunks json', JSON.stringify({
-    nested: flatten
-  }))
-  const depositWasmResult = await depositWasm(JSON.stringify(dep), JSON.stringify({
+  console.log('parsed deposit', JSON.stringify(args))
+  const depositWasmResult = await depositWasm(JSON.stringify(args), JSON.stringify({
     nested: flatten
   }));
 
   console.log({depositWasmResult})
   console.log('finish deposit')
+
+  return depositWasmResult;
 }
 
 // async function testMethod() {
@@ -103,4 +88,5 @@ export {
   // testMethod
   foo,
   deposit,
+  Deposit
 }
