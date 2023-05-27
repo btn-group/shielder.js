d458bd2c0dae1583de58cac925003d2e56ef0a7f
jsk28 <jsk28@sussex.ac.uk>
2023-05-27 15:07:49 +0200

    Callstack exceed fix

--- packages/shielder.js-sdk/src/browser.ts
+++ packages/shielder.js-sdk/src/browser.ts
@@ -1,5 +1,6 @@
-import init, {bar, 
-  deposit as depositWasm, 
+import init, {
+  bar,
+  deposit as depositWasm,
   // withdraw as withdrawWasm
 } from 'shielder-zk';
 
@@ -26,7 +27,7 @@ interface Deposit {
 async function readAllChunks(readableStream: any) {
   const reader = readableStream.getReader();
   const chunks = [];
-  
+
   let done, value;
   while (!done) {
     ({ value, done } = await reader.read());
@@ -48,21 +49,33 @@ async function readAllChunks(readableStream: any) {
 async function deposit(args: Deposit) {
   console.log('deposit');
 
+
+  console.log(JSON.stringify([[0, 0], [0, 1], [0, 3]]));
+
   const res = await fetch('https://bafybeifjsmmivrq2hxmujww7t2t3prbu3r2vffh7bq7pf6su2v5qgmzd4u.ipfs.w3s.link/ipfs/bafybeifjsmmivrq2hxmujww7t2t3prbu3r2vffh7bq7pf6su2v5qgmzd4u/deposit.pk.bytes')
   const chunks = (await readAllChunks(res.body));
   
-  const flatten: number[] = [];
+  // const flatten: number[] = [];
 
-  chunks.forEach(chunk => {
-    flatten.push(...chunk);
-  });
+  // chunks.forEach(chunk => {
+  //   sum += chunk.length;
+  //   flatten.push(...chunk);
+  // });
+  const flatten = chunks.reduce((acc, curr) => {
+    return [...acc, ...curr]
+  })
+
+  // for (let index = 0; index < chunks.length; index++) {
+  //   const element = chunks[index];
+  //   flatten.push(...element)
+  // }
 
   console.log('parsed deposit', JSON.stringify(args))
   const depositWasmResult = await depositWasm(JSON.stringify(args), JSON.stringify({
     nested: flatten
   }));
 
-  console.log({depositWasmResult})
+  console.log({ depositWasmResult })
   console.log('finish deposit')
 
   return depositWasmResult;
